4602b8fd681e8846215b75055ede08fde6d42768
mqunaibi <qunaibi@gmail.com>
2025-02-12 07:36:16 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="management",   
+    version="0.0.1",
+    description="Management App for managing meetings and related tasks in ERPNext",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Best Performance",
+    author_email="info@best-performance.co.uk",
+    license="GPL-3.0",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=("frappe",),
+)
