29a120f365c5c448965e03556bde788fbb5cba7e
mqunaibi <qunaibi@gmail.com>
2025-07-05 23:47:42 +0300

    ✨ إضافة Permit

---
+++ management/management/doctype/permit/test_permit.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Best Performance and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPermit(FrappeTestCase):
+	pass
